8db797f7852121efbffd3c89252abddc1a026823
vJxmes <45499511+vJxmes@users.noreply.github.com>
2018-11-30 15:24:07 -0600

    New first upload

---
+++ TheResponder.java
@@ -0,0 +1,22 @@
+package theResponder;
+
+import java.util.Scanner;
+
+public class TheResponder {
+
+	public static void main(String[] args) {
+		System.out.println("English or Spanish? (E/S)");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		
+		
+		char response = 'e';
+		
+		if(input.charAt(0) == response)
+		{
+			System.out.println("Hi, how are you?");
+		}
+
+	}
+
+}
